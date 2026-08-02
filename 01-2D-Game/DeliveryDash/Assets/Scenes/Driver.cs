a83aa32b48f5c03e98428c08f25786ffa68c2a31
Pariya Jankhani <pariya.jan2021@gmail.com>
2026-08-02 22:52:11 +0330

    move

--- 01-2D-Game/DeliveryDash/Assets/Scenes/Driver.cs
+++ 01-2D-Game/DeliveryDash/Assets/Scenes/Driver.cs
@@ -1,17 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+  [SerializeField] float speed = 0.1f;
+  [SerializeField] float rotate = 2f;
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,0,0.5f);
-        transform.Translate(0, 0.02f, 0); 
+       float  move = 0f; float arrow=0f;
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+            
+
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            arrow = 1f;
+           
+
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;
+           
+
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            arrow = -1f;
+            
+
+        }
+
+        float steerSpeed = arrow * rotate * Time.deltaTime, 
+            moveAmount = move * speed * Time.deltaTime;
+        
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0,moveAmount, 0);
+
     }
 }
